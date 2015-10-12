5640d2f5dff23d0fa0b8bad3cc5e8188ecae5d6e
iamdionysus <iamdionysus@gmail.com>
2015-10-12 14:24:21 +0800

    Add checkings

--- lib/git_change/author.rb
+++ lib/git_change/author.rb
@@ -1,4 +1,5 @@
 require "git_change/util"
+require "colorize"
 require "thor"
 
 module GitChange
@@ -17,8 +18,17 @@ module GitChange
     def clone_repo_bare
       # get remote url for current repo
       remote_url = GitChange::Util.git_remote_url
-      # inside it git clone bare
+      if remote_url.empty?
+        puts "You need to have remote repository to work on".colorize(:red)
+        return
+      end
+      if File.directory? "temp_repo"
+        puts "temp_repo directory already exists".colorize(:red)
+        return
+      end
+      # git clone bare to temp_repo to work on
       system "git clone --bare #{remote_url} temp_repo"
+      puts "First step: clone the bare repository is done".colorize(:green)
     end
 
     def run_change_author_script
@@ -26,16 +36,17 @@ module GitChange
       template "templates/change_author.tt", "temp_repo/change_author.sh"
       inside "temp_repo" do
         # run the script
-        system "./change_author.sh"
+        run "sh ./change_author.sh"
       end
     end
 
     def set_local_config
-      # run below
       # `git config user.name #{name}`
       # `git config user.email #{email}`
-      puts "changed user.name from #{old_name} to #{name}"
-      puts "changed user.email from #{old_email} to #{email}"
+      puts "change local config user.name from #{old_name} to #{name}".
+        colorize(:green)
+      puts "change local config user.email from #{old_email} to #{email}".
+        colorize(:green)
     end
   end
 end
